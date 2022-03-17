516f21ea1ace0451b1334638ec5764a598c049df
AninditaDas <anny.mca13@gmail.com>
2022-03-17 19:04:17 +0000

    changes on cicd demo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App Versio 5!')
+  res.send('CICD App Versio 5! for demo')
 })
 
 app.get('/status', (req, res) => {
